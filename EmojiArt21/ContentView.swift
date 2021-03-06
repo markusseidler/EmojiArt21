995f42f1b9bb7ff28a3f8d91b7d506cfdad6406b
Markus Seidler <mseidler79@gmail.com>
2021-03-06 19:26:40 +0800

    Initial Commit

---
+++ EmojiArt21/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt21
+//
+//  Created by Markus Seidler on 6/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiArt21/EmojiArt21App.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArt21App.swift
+//  EmojiArt21
+//
+//  Created by Markus Seidler on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArt21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
